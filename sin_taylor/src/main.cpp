0e2167f85660c64ca850b42fd9602ccdddabf217
Tigran Sargsyan <sargtigran@gmail.com>
2019-06-05 01:35:19 +0400

    Added simple body for main fuinction

--- sin_taylor/src/main.cpp
+++ sin_taylor/src/main.cpp
@@ -1,7 +1,30 @@
 
-// @brief Program for calculating SIN(x) by Taylor 
+/// @brief Program for calculating SIN(x) by Taylor 
+
+
+//
+#include <fstream>
+#include <iostream>
+
+
+/// function calculating SIN fiunction for given x
+double sin(double x)
+{
+
+}
 
 int main ()
 {
+    std::ifstream input("input.txt");
+
+    double x = 0;
+    if (input.is_open()) {
+        while (! input.eof()) {
+            input >> x;
+            std::cout << "sin(" << x << ") = ";
+            std::cout << sin(x) << std::endl;
+        }
+    }
+
     return 0;
 }
