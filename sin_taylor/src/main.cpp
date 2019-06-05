8032fcab4001d443db91858ab308a681d08a44e6
Tigran Sargsyan <sargtigran@gmail.com>
2019-06-05 07:32:56 +0000

    - Added body for sin(x) function

--- sin_taylor/src/main.cpp
+++ sin_taylor/src/main.cpp
@@ -1,28 +1,43 @@
 
-/// @brief Program for calculating SIN(x) by Taylor 
+/// @brief Program for calculating SIN(x) by Taylor series
 
 
-//
+///
 #include <fstream>
 #include <iostream>
+#include <cmath>
 
 
-/// function calculating SIN fiunction for given x
-double sin(double x)
+///factorial
+long factorial(int n)
 {
 
+}
+
+/// function calculating SIN function for given x
+double tsin(double x)
+{
+    ///
+    const double pi = 3.14159265358979323846;
+    const int N = 10;
+    double res = 0.0;
+    for (int n = 1; n < N; n += 2) {
+        double p = std::pow(x, n);
+        long f = factorial(n);
+        res += p / f;
+    }
 }
 
 int main ()
 {
     std::ifstream input("input.txt");
 
-    double x = 0;
+    double x = 0.0;
     if (input.is_open()) {
         while (! input.eof()) {
             input >> x;
             std::cout << "sin(" << x << ") = ";
-            std::cout << sin(x) << std::endl;
+            std::cout << tsin(x) << std::endl;
         }
     }
 
