c130c077bdd3e46f856ef53deb2911c9e919164f
Tigran Sargsyan <sargtigran@gmail.com>
2019-08-21 01:19:50 +0400

    - added not complete test code

--- shortest_path/src/shortest_path.cpp
+++ shortest_path/src/shortest_path.cpp
@@ -5,15 +5,14 @@
 #include <unordered_map>
 
 
-unsigned BellmaFordMethod(const Graph& g, const Vertex& start, std::vector<Vertex>& path)
+#include <iostream>
+
+unsigned BellmaFordMethod(const Graph& g, const Vertex& start, std::unordered_map<Vertex, Vertex>& path)
 {
-    const int max_weight = std::numeric_limits<int>::max();
+    const int max_weight = std::numeric_limits<int>::max() - 100; // dirty fix for prevent overflow
     
     std::unordered_map<Vertex, int> distance;
     
-    unsigned n = g.NumOfVertices();
-    path.resize(n);
-
     for (auto v : g.vertices()) {
         distance[v] = max_weight;
     }
@@ -23,9 +22,12 @@ unsigned BellmaFordMethod(const Graph& g, const Vertex& start, std::vector<Verte
         for (Edge e : g.edges()) {
             if (distance[e.first()] + e.weight() < distance[e.second()]) {
                 distance[e.second()] = distance[e.first()] + e.weight();
-                //path[e.second()] = e.first();
+                path[e.second()] = e.first();
             }
         }
+    }
+    for (auto d : distance) {
+        std::cout << "Vertex: " << d.first << " Weight: " <<  d.second << std::endl;
     }
     return 0;
 }
---
+++ shortest_path/test/test.cpp
@@ -0,0 +1,14 @@
+
+#include "shortest_path.hpp"
+#include "graph.hpp"
+
+int main ()
+{
+    VertSet verts = { 1, 2, 3, 4 };
+    Edge e(1, 2, 3);
+    EdgeSet edges;
+    edges.insert(e);
+
+    //Graph g(verts, edges);
+    return 0;
+}
